a174702b5ad82cb491add534c07aa5fd0e7c321f
cgbonn <merzbach@cs.uni-bonn.de>
2015-03-10 20:23:42 +0100

    small fix for reading BDIs

--- matlab/@btf/private/read_ubo_bdi.m
+++ matlab/@btf/private/read_ubo_bdi.m
@@ -125,7 +125,7 @@ function [data, meta] = read_ubo_bdi(fid, signature, header_flag)
     end
     
     % this mask indicates which of the chunks have been buffered by buffer_bdi
-    data.chunks_buffered = false(1, meta.num_chunks);
+    data.chunks_buffered = false(1, numel(meta.mask));
     data.num_chunks_in_buffer = 0;
     % disabling this leads to a hybrid mode where only those chunks that are not
     % buffered are read from file (this is still quite slow and therefore by
